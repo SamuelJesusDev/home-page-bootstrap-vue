cc23aeb56db67b66655fff541de4ef31f191c540
samuel <samuelcomputescience@gmail.com>
2022-05-31 09:41:30 -0300

    atualizando

--- script.js
+++ script.js
@@ -54,27 +54,9 @@ new Vue({
                 category: "web"
             },
             ],
-            empresas: [
-                { nome: 'Samsung', valorDeMercado: 50, CEO: 'Kim Hyun Suk', anoDeCriacao: 1938 },
-                { nome: 'Microsoft', valorDeMercado: 415, CEO: 'Satya Nadella', anoDeCriacao: 1975 },
-                { nome: 'Intel', valorDeMercado: 117, CEO: 'Brian Krzanich', anoDeCriacao: 1968 },
-                { nome: 'Facebook', valorDeMercado: 383, CEO: 'Mark Zuckerberg', anoDeCriacao: 2004 },
-                { nome: 'Spotify', valorDeMercado: 30, CEO: 'Daniel Ek', anoDeCriacao: 2006 },
-                { nome: 'Apple', valorDeMercado: 845, CEO: 'Tim Cook', anoDeCriacao: 1976 }
-            ],
-            cpf: '',
-            message: '',
-
         }
     },
-
-    // var cep = "22.000-000";
-    // var cepFormatado = false;
-    //this.cpf.indexOf('.')==2 && this.cpf.indexOf("-") == 6
-    // if(cep.indexOf(".") == 2 && cep.indexOf("-") == 6){
-    // cepFormatado = true;}
     computed: {
-
         filterByCategory() {
             return this.images.filter((image) => !image.category.indexOf(this.category));
         },
